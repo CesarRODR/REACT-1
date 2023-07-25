9da8b5e955aa768974fe8a8f8119ede75bc11ba4
CesarRODR <carodriguez17@gmail.com>
2023-07-24 22:51:57 -0300

    vrs-0.0.1
    Version uno de primer pre entrega, creando app con vite y componentes

---
+++ ENTREGA-1/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { NavBar } from "./components/NavBar";
+import { CartWidget } from "./components/CartWidget";
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NavBar />
+    
+  </React.StrictMode>,
+)
+
